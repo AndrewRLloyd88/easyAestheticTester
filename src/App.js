12c4278e0400afbf588f13dbe010f8de2a04e87e
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-15 09:21:47 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import addTwo from 'easyaesthetic';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    setNumber((n) => addTwo(number));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>The number is {number}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
